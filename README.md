1dae0dc3058841e2302e782a405da7e40a333c3f
Lylio <cxz@gmx.com>
2022-02-17 23:46:33 +0000

    binary search

--- README.md
+++ README.md
@@ -4,4 +4,40 @@
 # Algorithms
 
 ### Description
-A selection of classic algorithms written in Java which solve various software engineering tasks.
+
+The top 25 algorithms every programmer should know (according to an [article](https://medium.com/techie-delight/top-25-algorithms-every-programmer-should-know-373246b4881b) in **Medium.com**).
+
+- Binary Search Algorithm
+- Breadth First Search (BFS) Algorithm
+- Depth First Search (DFS) Algorithm
+- Merge Sort Algorithm
+- Quicksort Algorithm
+- Kruskal’s Algorithm
+- Floyd Warshall Algorithm
+- Dijkstra’s Algorithm
+- Bellman Ford Algorithm
+- Kadane’s Algorithm
+- Lee Algorithm
+- Flood Fill Algorithm
+- Floyd’s Cycle Detection Algorithm
+- Union Find Algorithm
+- Topological Sort Algorithm
+- KMP Algorithm
+- Insertion Sort Algorithm
+- Selection Sort Algorithm
+- Counting Sort Algorithm
+- Heap Sort Algorithm
+- Kahn’s Topological Sort Algorithm
+- Huffman Coding Compression Algorithm
+- Quickselect Algorithm
+- Boyer–Moore Majority Vote Algorithm
+- Euclid’s Algorithm
+
+
+<br/>
+
+#### Acknowledgements
+Based on the Medium.com article [Top 25 Algorithms Every Programmer Should Know](https://medium.com/techie-delight/top-25-algorithms-every-programmer-should-know-373246b4881b) writttend by *Coding Freak*.
+
+
+
---
+++ src/main/java/binarysearch/BinaryRecursive.java
@@ -0,0 +1,57 @@
+package binarysearch;
+
+public class BinaryRecursive {
+    // Recursive implementation of the binary search algorithm to return
+    // the position of `target` in subarray nums[left…right]
+    public static int binarySearch(int[] nums, int left, int right, int target) {
+        // Base condition (search space is exhausted)
+        if (left > right) {
+            return -1;
+        }
+
+        // find the mid-value in the search space and
+        // compares it with the target
+
+        int mid = (left + right) / 2;
+
+        // overflow can happen. Use below
+        // int mid = left + (right - left) / 2;
+
+        // Base condition (a target is found)
+        if (target == nums[mid]) {
+            return mid;
+        }
+
+        // discard all elements in the right search space,
+        // including the middle element
+        else if (target < nums[mid]) {
+            return binarySearch(nums, left, mid - 1, target);
+        }
+
+        // discard all elements in the left search space,
+        // including the middle element
+        else {
+            return binarySearch(nums, mid + 1, right, target);
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums = { 2, 5, 6, 8, 9, 10 };
+        int target = 5;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        int index = binarySearch(nums, left, right, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        }
+        else {
+            System.out.println("Element not found in the array");
+        }
+    }
+
+}
